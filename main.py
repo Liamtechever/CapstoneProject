d38c4e16abce0d738efb164650c195ee58beeb70
Liam <liamtechever@gmail.com>
2025-03-17 20:17:49 -0400

    Cleaned up main.py

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
+import re
 import json
+from dataclasses import dataclass
+
+from Model import Model, LocalModel, _available_models, OnlineModel
+from Settings import format_settings_for_modelpicker, load_settings
+from SpeechToText import transcribe_audio
+from AudioRecording import record_audio
+from Classifier import classify_prompt
+from ModelPicker import pick_model
+from TextToSpeech import play_tts
+from Classifier import Classification
 
 if __name__ == '__main__':
-    #initialize settings if doesn't exist
+    # Initialize settings if it doesn't exist
+    format_settings_for_modelpicker()
 
-    internet_response = False
-    settings = {"use_internet": internet_response}
+    play_tts("How can I help you today?", speaker="p251", speed=1, pitch=0)
 
-    with open("settings.json", "w") as file:
-        file.write(json.dumps(settings))
+    record_audio()
+    _prompt = transcribe_audio("response.wav")
+    print(f"Prompt: {_prompt}")
 
-    with open("settings.json", "r") as file:
-        settings = json.loads(file.read())
+    # Classify the prompt
+    prompt_classification = classify_prompt(prompt=_prompt)
+    print(prompt_classification)
 
-    # load settings
+    # Load user settings
+    user_settings = load_settings()
 
-    # Then do AI stuffs
+    # Pick the best models
+    best_models = pick_model(available_models=_available_models, prompt_classification=prompt_classification)
 
-
-
-
-
-    pass
+    print(best_models)
